e256a0917488135311de1b712062334c55c2706c
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-03-03 16:19:31 +0100

    DRX: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: hmd Model: Nokia 1.3 Script version: 3.0.0

---
+++ omni_DRX.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from DRX device
+$(call inherit-product, device/hmd/DRX/device.mk)
+
+PRODUCT_DEVICE := DRX
+PRODUCT_NAME := omni_DRX
+PRODUCT_BRAND := Nokia
+PRODUCT_MODEL := Nokia 1.3
+PRODUCT_MANUFACTURER := hmd
+
+PRODUCT_GMS_CLIENTID_BASE := android-hmd
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Drax_00WW-user 11 RKQ1.200928.002 00WW_2_37J release-keys"
+
+BUILD_FINGERPRINT := Nokia/Drax_00WW/DRX:11/RKQ1.200928.002/00WW_2_37J:user/release-keys
